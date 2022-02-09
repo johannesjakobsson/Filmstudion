604708ac251df3a64db785088fa45780906cecd5
jajo21 <89775394+jajo21@users.noreply.github.com>
2022-02-09 20:05:45 +0100

    Lagt in grundfiler och kopplat KlientGränssnittet

---
+++ wwwroot/script.js
@@ -0,0 +1,33 @@
+// TEST SCRIPT
+
+
+const getData = async (url) => {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+}
+
+const getFilms = async () => {
+    var data = await getData("api/films");
+    console.log(data);
+}
+
+
+getFilms();
+
+const getDataWithToken = async (url) => {
+    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJBZG1pbiIsImp0aSI6ImRiOGNjODU3LTBhMWItNDgyMC1iYmYyLTQ5Y2QyZDI3YmNlMCIsInVuaXF1ZV9uYW1lIjoiQWRtaW4iLCJleHAiOjE2NDQ1MTkyNDMsImlzcyI6Imh0dHBzLy9sb2NhbGhvc3Q6NTAwMSIsImF1ZCI6Imh0dHBzLy9sb2NhbGhvc3Q6NTAwMSJ9.YR4kHXP6UgdWIj9DaV4Az94tcVUxcdV-_AxrkFQzcmk';
+    const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Authorization': 'Bearer ' + token}
+    });
+    const data = await response.json();
+    return data;
+}
+
+const getFilmsWithToken = async() => {
+    var data = await getDataWithToken("api/films");
+    console.log(data);
+}
+getFilmsWithToken();
